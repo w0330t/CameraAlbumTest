06ae9dca45400f8d00f0a3eb0ae57375c65c4cc9
Blueness <w0330t@gmail.com>
2017-08-28 21:13:14 +0800

    Initial commit

---
+++ app/src/main/java/net/blueness/cameraalbumtest/MainActivity.kt
@@ -0,0 +1,62 @@
+package net.blueness.cameraalbumtest
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import org.jetbrains.anko.find
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private val FINAL_TAKE_PHOTO = 1
+    private var picture: ImageView? = null
+    private var imageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val takePhoto: Button = find(R.id.take_photo)
+        picture = find(R.id.picture)
+        takePhoto.setOnClickListener{
+            val outputImage = File(externalCacheDir, "output_image.jpg")
+            if(outputImage.exists()) {
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            imageUri = if(Build.VERSION.SDK_INT >= 24){
+                FileProvider.getUriForFile(this, "net.blueness.cameraalbumtest.fileprovider", outputImage)
+            } else {
+                Uri.fromFile(outputImage)
+            }
+
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, FINAL_TAKE_PHOTO)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            FINAL_TAKE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+
+                    val bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri))
+                    Log.d("MainActivity", bitmap.width.toString())
+                    Log.d("MainActivity", bitmap.height.toString())
+                    picture!!.setImageBitmap(bitmap)
+//                    Log.d("MainActivity", picture.toString())
+                }
+        }
+    }
+}
