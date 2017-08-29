684e052f704b1a69703813843b53bd4f970ed492
Blueness <w0330t@gmail.com>
2017-08-29 10:47:47 +0800

    complete

--- app/src/main/java/net/blueness/cameraalbumtest/MainActivity.kt
+++ app/src/main/java/net/blueness/cameraalbumtest/MainActivity.kt
@@ -1,24 +1,30 @@
 package net.blueness.cameraalbumtest
 
+import android.annotation.TargetApi
 import android.app.Activity
+import android.content.ContentUris
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.content.FileProvider
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import org.jetbrains.anko.find
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private val FINAL_TAKE_PHOTO = 1
-    val FINAL_CHOOSE_PHOTO = 2
+    private val FINAL_CHOOSE_PHOTO = 2
     private var picture: ImageView? = null
     private var imageUri: Uri? = null
 
@@ -48,7 +54,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         chooseFromAlbum.setOnClickListener{
+            val checkSelfPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if (checkSelfPermission != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            }
+            else{
+                openAlbum()
+            }
+        }
+    }
 
+    private fun openAlbum(){
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, FINAL_CHOOSE_PHOTO)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1 -> 
+                if (grantResults.isNotEmpty() && grantResults.get(0) ==PackageManager.PERMISSION_GRANTED){
+                    openAlbum()
+                }
+                else {
+                    Toast.makeText(this, "You deied the permission", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 
@@ -60,6 +91,71 @@ class MainActivity : AppCompatActivity() {
                     val bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri))
                     picture!!.setImageBitmap(bitmap)
                 }
+            FINAL_CHOOSE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+//                        4.4以上
+                        handleImageOnKitkat(data)
+                    }
+                    else{
+//                        4.4以下
+                        handleImageBeforeKitkat(data)
+                    }
+                }
+        }
+    }
+
+
+    @TargetApi(19)
+    private fun handleImageOnKitkat(data: Intent?) {
+        var imagePath: String? = null
+        val uri = data!!.data
+        if (DocumentsContract.isDocumentUri(this, uri)){
+//            document类型的Uri，用document id处理
+            val docId = DocumentsContract.getDocumentId(uri)
+            if ("com.android.providers.media.documents" == uri.authority){
+                val id = docId.split(":")[1]
+                val selsetion = MediaStore.Images.Media._ID + "=" + id
+                imagePath = imagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selsetion)
+            }
+            else if ("com.android.providers.downloads.documents" == uri.authority){
+                val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(docId))
+                imagePath = imagePath(contentUri, null)
+            }
+        }
+        else if ("content".equals(uri.scheme, ignoreCase = true)){
+//            content类型Uri 普通方式处理
+            imagePath = imagePath(uri, null)
+        }
+        else if ("file".equals(uri.scheme, ignoreCase = true)){
+            imagePath = uri.path
+        }
+        displayImage(imagePath)
+    }
+
+    //    没4.4的设备，略过
+    private fun handleImageBeforeKitkat(data: Intent?) {}
+
+    private fun imagePath(uri: Uri?, selection: String?): String {
+        var path: String? = null
+//        通过Uri和selection获取路径
+        val cursor = contentResolver.query(uri, null, selection, null, null )
+        if (cursor != null){
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+            }
+            cursor.close()
+        }
+        return path!!
+    }
+
+    private fun displayImage(imagePath: String?){
+        if (imagePath != null) {
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            picture?.setImageBitmap(bitmap)
+        }
+        else {
+            Toast.makeText(this, "Failed to get image", Toast.LENGTH_SHORT).show()
         }
     }
 }
